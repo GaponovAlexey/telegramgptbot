e453879af2e1394e37c9d1510132c1d8dc819d72
kekelberi <alexei.gaponov@gmail.com>
2023-01-16 20:40:53 -0600

    save

--- main.go
+++ main.go
@@ -14,12 +14,14 @@ import (
 
 func main() {
 	err := godotenv.Load()
+
 	if err != nil {
 		panic(err)
 	}
+
 	apiKey := os.Getenv("API")
 	url := "https://api.openai.com/v1/engines/davinci/completions"
-	prompt := "Привет"
+	prompt := "Сколько время в Winnipeg"
 	maxTokens := 60
 	temperature := 0.5
 	stop := []string{"You:"}
@@ -37,10 +39,19 @@ func main() {
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
 	res, _ := http.DefaultClient.Do(req)
+	body, _ := ioutil.ReadAll(res.Body)
+
+	type OpenAIResponse struct {
+		Choices []struct {
+			Text string `json:"text"`
+		} `json:"choices"`
+	}
+
+	var response OpenAIResponse
+
+	json.Unmarshal([]byte(body), &response)
 
 	defer res.Body.Close()
 
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(string(body))
+	fmt.Println(fmt.Sprintf("%s", response.Choices))
 }
